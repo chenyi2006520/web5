fb4344d1bd8cf0621e23ead268fe9e7fef3563e2
chenyi2006520 <chenyi20065202outlook.com>
2016-06-15 16:14:09 +0800

    重新配置

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 // /*数据库配置*/
 'DB_TYPE' => 'MYSQL', //数据库类型
 'DB_HOST' => 'localhost', // 服务器地址
-'DB_NAME' => 'bamengDB', // 数据库名
+'DB_NAME' => 'bamengdb', // 数据库名
 'DB_USER' => 'root', // 用户名
 'DB_PWD' => '1988chenyi@', // 密码
 'DB_PORT' => '3306', // 端口
